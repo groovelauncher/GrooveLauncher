65ab2e3fb9ff6bbb0d675337e9724b1028e45288
Berkay Tumal <themetrocrash@gmail.com>
2024-12-20 04:23:41 +0300

    save us 0.5.2 from 0.5.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "web.bmdominatezz.gravy"
         minSdk = 24
         targetSdk = 34
-        versionCode = 51
-        versionName = "0.5.1-beta.5"
+        versionCode = 52
+        versionName = "0.5.2-beta.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
         // Add the API key

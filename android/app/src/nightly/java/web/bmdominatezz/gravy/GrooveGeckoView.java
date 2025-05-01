f9c5951dd071a29a2e522034c9cd3c0bcb84cf93
Berkay Tumal <83873781+berkaytumal@users.noreply.github.com>
2025-05-01 05:28:30 +0000

    fix: add GrooveGeckoView class for all build flavors

---
+++ android/app/src/nightly/java/web/bmdominatezz/gravy/GrooveGeckoView.java
@@ -0,0 +1,1 @@
+package web.bmdominatezz.gravy; import android.content.Context; import android.view.View; public class GrooveGeckoView extends View { public GrooveGeckoView(Context context) { super(context); } }

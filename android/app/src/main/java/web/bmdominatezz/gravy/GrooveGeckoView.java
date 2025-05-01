748553a599d0d2b0aafc2112646d6172e2193bfa
Berkay Tumal <83873781+berkaytumal@users.noreply.github.com>
2025-05-01 05:12:51 +0000

    feat: implement GrooveGeckoView class for GeckoView integration

---
+++ android/app/src/main/java/web/bmdominatezz/gravy/GrooveGeckoView.java
@@ -0,0 +1,9 @@
+package web.bmdominatezz.gravy;
+import android.content.Context;
+import android.view.View;
+
+public class GrooveGeckoView extends View {
+    public GrooveGeckoView(Context context) {
+        super(context);
+    }
+}

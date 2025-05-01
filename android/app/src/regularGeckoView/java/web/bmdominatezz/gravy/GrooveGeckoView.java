a37a7f7a020e4e49a51c3d06297bea0c521904f2
Berkay Tumal <83873781+berkaytumal@users.noreply.github.com>
2025-05-01 05:05:52 +0000

    feat: add GrooveGeckoView class for GeckoView integration in both nightly and regular builds

---
+++ android/app/src/regularGeckoView/java/web/bmdominatezz/gravy/GrooveGeckoView.java
@@ -0,0 +1,12 @@
+package web.bmdominatezz.gravy;
+import android.content.Context;
+
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
+
+public class GrooveGeckoView extends GeckoView{
+    public GrooveGeckoView(Context context) {
+        super(context);
+    }
+}

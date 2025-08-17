ad9a44fb5e56d4140f60e833d91d09a61c36c8e2
Berkay Tumal <berkaytml34@gmail.com>
2025-08-17 23:12:18 +0300

    build(android): add debug signing config to release build
    Enable debug signing for release builds to simplify testing during development

--- android/gravyservicestest/build.gradle.kts
+++ android/gravyservicestest/build.gradle.kts
@@ -22,6 +22,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
